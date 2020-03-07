bad8ce67946f9327c613edaaa68839e50f32ea9e
unknown <ali.warishaa@gmail.com>
2020-03-08 02:56:21 +0500

    notifications

--- App/Containers/PushNotifContainer/PushNotifContainer.tsx
+++ App/Containers/PushNotifContainer/PushNotifContainer.tsx
@@ -4,7 +4,8 @@ import PushNotification from "react-native-push-notification";
 
 export default class PushNotifContainer extends Component{
     componentDidMount(){
-        PushNotification.configure({
+        
+      PushNotification.configure({
             // (optional) Called when Token is generated (iOS and Android)
             onRegister: function(token) {
               console.log("TOKEN:", token);
